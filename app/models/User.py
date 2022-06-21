48b1de722216df6cb391455abde50257f9dea5ee
Kyle Canoy <kcanoy@uw.edu>
2022-06-20 17:44:56 -0700

    fix typo in User.py

--- app/models/User.py
+++ app/models/User.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String
 
 class User(Base):
   __tablename__ = "users"
-  if = Column(Integer, primary_key=True)
+  id = Column(Integer, primary_key=True)
   username = Column(String(50), nullable=False)
   email = Column(String(50), nullable=False, unique=True)
   password = Column(String(100), nullable=False)
