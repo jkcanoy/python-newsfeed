bffd1358743d0f1d753a61608b1d362d757f48f8
Kyle Canoy <kcanoy@uw.edu>
2022-06-27 16:49:12 -0700

    create filter to format urls

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -4,3 +4,8 @@ def format_date(date):
 from datetime import datetime
 print(format_date(datetime.now()))
 
+def format_url(url):
+  return url.replace("http://", "").replace("https://", "").replace("www.", "").split("/")[0].split("?")[0]
+
+print(format_url("http://google.com/test/"))
+print(format_url("https://www.google.com?q=test"))
